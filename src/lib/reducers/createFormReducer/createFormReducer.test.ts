c869b8ed9e3eb8c27302f4f2f8ea1083483d348a
Gustaf Lundström <gustaflundstrom@Gustafs-MBP.lan>
2021-03-28 11:37:27 +0200

    Add cases for other actions in form reducer.

--- src/lib/reducers/createFormReducer/createFormReducer.test.ts
+++ src/lib/reducers/createFormReducer/createFormReducer.test.ts
@@ -1,35 +1,30 @@
-import { fieldBase } from 'lib/schema/createBaseField/__fixtures__'
-import defaultFieldValues from 'lib/schema/defaultFieldValues'
-import { FormState } from 'lib/schema/types'
+import { initFormState } from 'lib/helpers/initFormState'
+import { schemaCreator } from 'lib/schema'
 import { createFormReducer } from '.'
-import { updateFieldValue } from '../../actions/actions'
+import { clearForm, resetForm, updateFieldValue } from '../../actions/actions'
 
-const initialState: FormState<{
-  name: 'text'
-  age: 'number'
+const simpleSchemaCreator = schemaCreator({
+  name: 'text',
+  age: 'number',
   isAlive: 'checkbox'
-}> = {
-  name: {
-    ...defaultFieldValues.text,
-    ...fieldBase,
-    value: ''
-  },
-  age: {
-    ...defaultFieldValues.number,
-    ...fieldBase,
-    value: 0
-  },
-  isAlive: {
-    ...defaultFieldValues.checkbox,
-    ...fieldBase,
-    value: false
-  }
+})
+
+const setup = (...values: Parameters<typeof simpleSchemaCreator>) => {
+  const formSchema = simpleSchemaCreator(...values)
+  const initialState = initFormState(formSchema)
+  const formReducer = createFormReducer(formSchema)
+  return { formReducer, initialState }
 }
 
-const formReducer = createFormReducer(initialState)
-
 describe('formReducer', () => {
-  test('UPDATE_VALUE', () => {
+  test('bad action', () => {
+    const { formReducer, initialState } = setup()
+    expect(formReducer(initialState, { type: 'WAZZAA' } as any)).toEqual(
+      initialState
+    )
+  })
+  test('updateFieldValue', () => {
+    const { formReducer, initialState } = setup()
     expect(
       formReducer(
         initialState,
@@ -38,6 +33,55 @@ describe('formReducer', () => {
     ).toEqual({
       ...initialState,
       name: { ...initialState.name, value: 'hello' }
+    })
+  })
+  test('resetForm', () => {
+    const { formReducer, initialState } = setup({
+      name: 'test',
+      age: 25,
+      isAlive: true
+    })
+    expect(formReducer(initialState, resetForm())).toEqual(initialState)
+  })
+  test('clearForm', () => {
+    const { formReducer, initialState } = setup({
+      name: 'test',
+      age: 25,
+      isAlive: true
+    })
+    expect(
+      formReducer(
+        {
+          ...initialState,
+          name: {
+            ...initialState.name,
+            error: 'error!'
+          },
+          age: {
+            ...initialState.age,
+            error: 'error!'
+          },
+          isAlive: {
+            ...initialState.isAlive,
+            error: 'error!'
+          }
+        },
+        clearForm()
+      )
+    ).toEqual({
+      ...initialState,
+      name: {
+        ...initialState.name,
+        value: ''
+      },
+      age: {
+        ...initialState.age,
+        value: 0
+      },
+      isAlive: {
+        ...initialState.isAlive,
+        value: false
+      }
     })
   })
 })
--- src/lib/reducers/createFormReducer/createFormReducer.ts
+++ src/lib/reducers/createFormReducer/createFormReducer.ts
@@ -1,6 +1,15 @@
-import { Reducer } from 'react'
-import { UPDATE_VALUE, CLEAR_FORM, FormActions } from '../../actions/actions'
-import { FieldTypeSchema, FormState } from '../../schema/types'
+import type { Reducer } from 'react'
+import { initFormState } from 'lib/helpers/initFormState'
+import defaultFieldValues from 'lib/schema/defaultFieldValues'
+import type { FieldTypeSchema, FormSchema, FormState } from '../../schema/types'
+import {
+  UPDATE_VALUE,
+  CLEAR_FORM,
+  VALIDATE_FIELD,
+  VALIDATE_FORM,
+  RESET_FORM,
+  FormActions
+} from '../../actions/actions'
 
 export type FormReducer<T extends FieldTypeSchema> = Reducer<
   FormState<T>,
@@ -8,7 +17,7 @@ export type FormReducer<T extends FieldTypeSchema> = Reducer<
 >
 
 const createFormReducer = <T extends FieldTypeSchema>(
-  initialState: FormState<T>
+  formSchema: FormSchema<T>
 ): FormReducer<T> => (state, action) => {
   switch (action.type) {
     case UPDATE_VALUE: {
@@ -16,12 +25,37 @@ const createFormReducer = <T extends FieldTypeSchema>(
         ...state,
         [action.payload.key]: {
           ...state[action.payload.key],
-          value: action.payload.value
+          value:
+            action.payload.value === undefined &&
+            formSchema[action.payload.key].type === 'checkbox'
+              ? !state[action.payload.key].value
+              : action.payload.value
         }
       }
     }
     case CLEAR_FORM: {
-      return initialState
+      return Object.entries(state).reduce<FormState<T>>(
+        (acc, [key, values]) => ({
+          ...acc,
+          [key]: {
+            ...values,
+            value: defaultFieldValues[formSchema[key].type].value,
+            error: ''
+          }
+        }),
+        state
+      )
+    }
+    case VALIDATE_FORM: {
+      return state
+    }
+    case VALIDATE_FIELD: {
+      return {
+        ...state
+      }
+    }
+    case RESET_FORM: {
+      return initFormState(formSchema)
     }
     default: {
       return state
