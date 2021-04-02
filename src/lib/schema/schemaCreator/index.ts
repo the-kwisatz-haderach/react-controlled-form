f59f9f1e6d2d4b881ec978873b8623c328f0ce58
Gustaf Lundström <gustaflundstrom@Gustafs-MBP.lan>
2021-04-02 12:23:11 +0200

    Remove redundant modules.

--- src/lib/schema/schemaCreator/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as schemaCreator } from './schemaCreator'
--- src/stories/formSchema.ts
+++
@@ -1,7 +0,0 @@
-import { schemaCreator } from '../lib/schema'
-
-export const simpleSchemaCreator = schemaCreator({
-  name: 'text',
-  age: 'number',
-  isLeet: 'checkbox'
-})
